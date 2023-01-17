5d89ab1b5eb4c4c08ae1a17ba8a03eed91b6f1e8
GinaCamelia <92101771+GinaCamelia@users.noreply.github.com>
2023-01-17 15:56:52 +0000

    started by asking user to choose the length of characters for the password and then continued to do requirements the user needs to meet through if-else statements; when given to choice to choose, user also has requirements to meet. Stored the user's choice in an object and then called the object

--- script.js
+++ script.js
@@ -10,10 +10,41 @@ var lowerCasedCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k
 // Array of uppercase characters to be included in password
 var upperCasedCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
+
 // Function to prompt user for password options
 function getPasswordOptions() {
+    //Prompts user for password length
+    let passwordLength = parseInt(prompt('Enter the desired password length between 10 and 64 (included), please:'));
 
-}
+    // These lines checks if the entered password length is not a number or is less than 10 or greater than 64 characters
+    if (isNaN(passwordLength) === true || passwordLength < 10 || passwordLength > 64) {
+        alert('Password length must be a NUMBER between 10 and 64 (included). Please try again.');
+        return;
+    } else {        
+        // Continue once password length requirements are met
+        confirmSpecialChar = confirm('Will this contain special characters?');
+        confirmNumber = confirm('Will this contain numeric characters?');
+        confirmLowerCase = confirm('Will this contain lowercase characters?');
+        confirmUpperCase = confirm('Will this contain uppercase characters?');
+    };
+
+    // If user input is invalid check
+    if (!confirmSpecialChar && !confirmNumber && !confirmLowerCase && !confirmUpperCase) {
+        alert('You must select at least one option. Please try again.');
+        return;
+    }
+
+    //Create an object to hold the user's choices from the input
+    let userInput = {
+        passwordLength: passwordLength,
+        confirmSpecialChar: confirmSpecialChar,
+        confirmNumber: confirmNumber,
+        confirmLowerCase: confirmLowerCase,
+        confirmUpperCase: confirmUpperCase
+    };
+    console.log(userInput);
+    return userInput;
+};
 
 // Function for getting a random element from an array
 function getRandom(arr) {}
